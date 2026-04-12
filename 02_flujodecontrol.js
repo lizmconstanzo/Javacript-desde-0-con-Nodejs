11f361495ca6c1005248c141a31fe9f522f376e6
Liz Constanzo <lizconstanzo23@gmail.com>
2026-04-11 23:23:31 -0400

    Ejercicios de flujo de control

--- 02_flujodecontrol.js
+++ 02_flujodecontrol.js
@@ -58,3 +58,53 @@ switch (DiadeSemana) {
         console.log("El dia ingresado no es valido");
         break;
 }
+
+
+// Escribir un programa que segun el total de la compra, se agregue un valor de envio.
+// Si la compra es menor o igual a los $10 se agregara un gasto de envio de $3
+// Si la compra es menor o igual a los $20 y mayor a los $10 se agregara un gasto de envio de $5
+// Si la compra es menor o igual a $50 y mayor a los $20 se agregara un gasto de envio de $7
+// Si la compra supera los $50 el gasto de envio sera gratuito
+
+// El programa debe ser escrito dos veces, uno con la declaracion if else y otra con la declaracion
+// switch
+
+
+let totaldecompra = 40;
+let envio = 0;
+
+if (totaldecompra <= 10){
+    envio = 3;
+}
+else if(totaldecompra <=20) {
+    envio = 5;
+}
+else if(totaldecompra <=50){
+    envio = 7;
+}
+else{
+    envio = 0;
+}
+
+console.log("El total es: ", totaldecompra + envio);
+
+let totalFinal = 20;
+
+switch (true) {
+    case totalFinal <=10:
+        envio = 3;
+        console.log("el total final es: ", totalFinal + envio);
+        break;
+    case totalFinal <=20:
+        envio = 5;
+        console.log("el total final es: ", totalFinal + envio);
+        break;
+    case totalFinal <=50:
+        envio = 7;
+        console.log("el total final es: ", totalFinal + envio);
+        break;
+
+    default:
+        console.log("el envio es gratis");
+        break;
+}
